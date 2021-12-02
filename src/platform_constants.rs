a1f72197d99bbd7b9388f787dc3342f83b244167
Maximilian Ammann <max@maxammann.org>
2021-12-02 20:07:12 +0100

    Refine platform constants for webgl

--- src/platform_constants.rs
+++ src/platform_constants.rs
@@ -1,5 +1,7 @@
 
-#[cfg(target_arch = "wasm32")]
+#[cfg(all(target_arch = "wasm32", not(feature = "web-webgl")))]
 pub const COLOR_TEXTURE_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Bgra8Unorm;
+#[cfg(all(target_arch = "wasm32", feature = "web-webgl"))]
+pub const COLOR_TEXTURE_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Rgba8UnormSrgb;
 #[cfg(not(target_arch = "wasm32"))]
 pub const COLOR_TEXTURE_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Bgra8UnormSrgb;
