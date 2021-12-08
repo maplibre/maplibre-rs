95f6447108578e7278a0108bd59967c88706398f
Maximilian Ammann <max@maxammann.org>
2021-12-08 18:12:15 +0100

    Start style-spec crate

---
+++ libs/style_spec/build.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io::BufReader;
+use serde_json::Value;
+
+fn generate_type_def() -> Option<u32> {
+    let mut f = File::open("style-spec-v8.json").unwrap();
+    let mut reader = BufReader::new(f);
+    let result = serde_json::from_reader::<_, Value>(&mut reader).unwrap();
+
+    let spec_root = result.as_object()?;
+    let version = &spec_root["$version"].as_i64()?;
+    let root = &spec_root["$root"].as_object()?;
+
+    for x in spec_root {
+
+    }
+
+    println!("cargo:warning={:?}", version);
+
+    Some(5)
+}
+
+
+fn main() {
+    generate_type_def();
+}
