d2b2abc827c3602f4841de029f6e5d7fe9d27a18
Maximilian Ammann <max@maxammann.org>
2021-12-09 16:53:49 +0100

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-use winit::event_loop::EventLoop;
-use winit::window::WindowBuilder;
-
 mod fps_meter;
 mod platform;
 mod render;
@@ -9,15 +6,3 @@ mod setup;
 
 #[cfg(target_arch = "wasm32")]
 mod web;
-
-fn main() {
-    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
-
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_title("A fantastic window!")
-        .build(&event_loop)
-        .unwrap();
-
-    pollster::block_on(setup::setup(window, event_loop));
-}
