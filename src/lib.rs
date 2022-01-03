0559f69a9444b204471b9701494b5ee3771619de
Maximilian Ammann <max@maxammann.org>
2022-01-03 10:34:52 +0100

    Fix clippy in CI

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,12 @@ mod input;
 mod platform;
 
 pub(crate) mod example;
-pub(crate) mod io;
 pub(crate) mod render;
 pub(crate) mod util;
 
+// Used from outside to initialize mapr
+pub mod io;
 pub mod main_loop;
+
+// Used for benchmarking
 pub mod tesselation;
