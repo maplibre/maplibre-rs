eea85e7fa54b90f2ef3108fae7ad909ebf2653d0
Maximilian Ammann <max@maxammann.org>
2022-01-08 22:29:08 +0100

    Add error enum

---
+++ src/error.rs
@@ -0,0 +1,4 @@
+#[derive(Debug)]
+pub enum Error {
+    Network(String),
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod input;
 mod platform;
 
 pub(crate) mod coords;
+pub(crate) mod error;
 pub(crate) mod example;
 pub(crate) mod render;
 pub(crate) mod util;
