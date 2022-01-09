4b48c75ab1bdfe5f6df098ed003bdd78abc30271
Maximilian Ammann <max@maxammann.org>
2022-01-09 19:38:08 +0100

    Fix service worker path

--- web/index.ts
+++ web/index.ts
@@ -47,7 +47,7 @@ const start = async () => {
 
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/service-worker.js');
+            navigator.serviceWorker.register('./service-worker.js');
         });
     }
 
