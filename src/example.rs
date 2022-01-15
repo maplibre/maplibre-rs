a91fb8a25c28316ecdabec21a66f5da3cbc411dd
Maximilian Ammann <max@maxammann.org>
2022-01-15 15:25:24 +0100

    Choose reasonable size

--- src/example.rs
+++ src/example.rs
@@ -1,12 +1,13 @@
 use crate::io::worker_loop::WorkerLoop;
 
-pub const MUNICH_X: u32 = 17421;
-pub const MUNICH_Y: u32 = 11360;
+pub const MUNICH_X: u32 = 17425;
+pub const MUNICH_Y: u32 = 11365;
 pub const MUNICH_Z: u8 = 15;
 
 pub fn fetch_munich_tiles(worker_loop: &WorkerLoop) {
-    for x in 0..15 {
-        for y in 0..15 {
+    // This size matches the amount of tiles which are loaded on zoom 15 on FHD
+    for x in 0..8 {
+        for y in 0..5 {
             worker_loop.fetch((MUNICH_X + x, MUNICH_Y + y, MUNICH_Z).into())
         }
     }
