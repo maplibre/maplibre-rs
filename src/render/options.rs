c01aba5a630a494a7df5adb0a97f2d0efdd66a93
Maximilian Ammann <max@maxammann.org>
2022-03-04 14:49:25 +0100

    Remove unused instance

--- src/render/options.rs
+++ src/render/options.rs
@@ -9,4 +9,3 @@ pub const FEATURE_METADATA_BUFFER_SIZE: BufferAddress = 1024 * 1024 * 32;
 pub const INDICES_BUFFER_SIZE: BufferAddress = 1024 * 1024 * 16;
 
 pub const TILE_META_COUNT: BufferAddress = 1024 * 24;
-pub const TILE_MASK_INSTANCE_COUNT: BufferAddress = 512; // FIXME: Pick reasonable size
