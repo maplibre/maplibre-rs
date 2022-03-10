5bebe15f54ce91e008fece299eadb77ca9cbc505
Maximilian Ammann <max@maxammann.org>
2022-03-10 15:04:47 +0100

    Move example to main module

--- examples/desktop.rs
+++ examples/desktop.rs
@@ -1,44 +1,5 @@
-use mapr::main_loop;
-use std::sync::mpsc::channel;
-use std::thread;
-use tokio::runtime::Handle;
+use mapr::platform::mapr_generic_main;
 
-use mapr::io::tile_cache::TileCache;
-use mapr::io::web_tile_fetcher::WebTileFetcher;
-use mapr::io::workflow::{DownloadTessellateLoop, TileRequestDispatcher, Workflow};
-use mapr::io::{HttpFetcherConfig, TileFetcher};
-use tokio::task;
-use winit::event_loop::EventLoop;
-use winit::window::WindowBuilder;
-
-#[tokio::main]
-async fn main() {
-    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
-
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_title("A fantastic window!")
-        .build(&event_loop)
-        .unwrap();
-
-    let workflow = Workflow::create();
-    let download_tessellate_loop = workflow.download_tessellate_loop;
-    let tile_request_dispatcher = workflow.tile_request_dispatcher;
-    let layer_result_receiver = workflow.layer_result_receiver;
-
-    let join_handle = task::spawn_blocking(move || {
-        Handle::current().block_on(async move {
-            download_tessellate_loop.run_loop().await;
-        });
-    });
-
-    main_loop::setup(
-        window,
-        event_loop,
-        Box::new(tile_request_dispatcher),
-        Box::new(layer_result_receiver),
-        Box::new(TileCache::new()),
-    )
-    .await;
-    join_handle.await.unwrap()
+fn main() {
+    mapr_generic_main()
 }
--- src/platform/generic.rs
+++ src/platform/generic.rs
@@ -1,6 +1,36 @@
 //! Module which is used if android, apple and web is not used.
 
+use crate::io::tile_cache::TileCache;
+use crate::io::workflow::Workflow;
+use crate::main_loop;
 pub use std::time::Instant;
+use tokio::runtime::Handle;
+use tokio::task;
+use winit::event_loop::EventLoop;
+use winit::window::WindowBuilder;
 
 // Vulkan/OpenGL
 pub const COLOR_TEXTURE_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Bgra8UnormSrgb;
+
+#[tokio::main]
+pub async fn mapr_generic_main() {
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("A fantastic window!")
+        .build(&event_loop)
+        .unwrap();
+
+    let mut workflow = Workflow::create();
+    let download_tessellate_loop = workflow.take_download_loop();
+
+    let join_handle = task::spawn_blocking(move || {
+        Handle::current().block_on(async move {
+            download_tessellate_loop.run_loop().await;
+        });
+    });
+
+    main_loop::setup(window, event_loop, Box::new(workflow)).await;
+    join_handle.await.unwrap()
+}
