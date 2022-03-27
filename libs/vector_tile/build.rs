e46278478563ef35153f1f7728ccff98c2b0744a
Maximilian Ammann <max@maxammann.org>
2022-03-27 15:06:20 +0200

    Upgrade protobuf and use minimal runtime

--- libs/vector_tile/build.rs
+++ libs/vector_tile/build.rs
@@ -1,8 +1,15 @@
+use protobuf_codegen_pure::Customize;
 use std::path::PathBuf;
 
 fn main() {
     let out_path = PathBuf::from("src/protos");
     protobuf_codegen_pure::Codegen::new()
+        .customize(Customize {
+            //carllerche_bytes_for_bytes: Some(true),
+            //carllerche_bytes_for_string: Some(true),
+            lite_runtime: Some(true),
+            ..Default::default()
+        })
         .out_dir(out_path)
         .inputs(&["spec/2.1/vector_tile.proto"])
         .include("spec/2.1")
