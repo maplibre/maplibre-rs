332074bfade4aae8750dbf94ba5c95c5be793812
Maximilian Ammann <max@maxammann.org>
2022-04-18 20:24:33 +0200

    Remove benchmarks for now

--- benches/tessellation.rs
+++ benches/tessellation.rs
@@ -3,8 +3,6 @@ use lyon::tessellation::VertexBuffers;
 use mapr::benchmarking::io::static_tile_fetcher::StaticTileFetcher;
 use mapr::benchmarking::tessellation::Tessellated;
 use std::io::Cursor;
-use vector_tile::parse_tile_reader;
-use vector_tile::tile::Layer;
 
 const MUNICH_X: u32 = 17425;
 const MUNICH_Y: u32 = 11365;
@@ -13,7 +11,7 @@ const MUNICH_Z: u8 = 15;
 fn parse_tile(c: &mut Criterion) {
     let fetcher = StaticTileFetcher::new();
 
-    c.bench_function("parse", |b| {
+    /*    c.bench_function("parse", |b| {
         b.iter(|| {
             parse_tile_reader(&mut Cursor::new(
                 fetcher
@@ -22,11 +20,11 @@ fn parse_tile(c: &mut Criterion) {
             ))
             .expect("failed to load tile")
         })
-    });
+    });*/
 }
 
 fn tessellate_tile(c: &mut Criterion) {
-    let fetcher = StaticTileFetcher::new();
+    /*    let fetcher = StaticTileFetcher::new();
     let tile = parse_tile_reader(&mut Cursor::new(
         fetcher
             .sync_fetch_tile(&(MUNICH_X, MUNICH_Y, MUNICH_Z).into())
@@ -39,7 +37,7 @@ fn tessellate_tile(c: &mut Criterion) {
         let _: (VertexBuffers<_, u32>, _) = layer.tessellate().unwrap();
     }
 
-    c.bench_function("tessselate", |b| b.iter(|| tessselate(layer)));
+    c.bench_function("tessselate", |b| b.iter(|| tessselate(layer)));*/
 }
 
 criterion_group!(benches, parse_tile, tessellate_tile);
