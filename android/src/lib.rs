c2744a6d3ee42d3265885289dfea6242f49d3fad
Maximilian Ammann <max@maxammann.org>
2022-05-07 15:56:35 +0200

    Fix android and apple

--- android/src/lib.rs
+++ android/src/lib.rs
@@ -5,6 +5,7 @@ use maplibre::platform::http_client::ReqwestHttpClient;
 use maplibre::platform::schedule_method::TokioScheduleMethod;
 use maplibre::window::FromWindow;
 use maplibre::MapBuilder;
+use maplibre_winit::winit::{WinitEventLoop, WinitMapWindow, WinitWindow};
 use std::ffi::CString;
 
 #[cfg(not(target_os = "android"))]
@@ -14,7 +15,8 @@ compile_error!("android works only on android.");
 pub fn android_main() {
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
-    MapBuilder::from_window("A fantastic window!")
+    let builder: MapBuilder<WinitMapWindow, _, _, _> = MapBuilder::new();
+    builder
         .with_http_client(ReqwestHttpClient::new(None))
         .with_schedule_method(TokioScheduleMethod::new())
         .build()
--- apple/src/lib.rs
+++ apple/src/lib.rs
@@ -2,6 +2,7 @@ use maplibre::platform::http_client::ReqwestHttpClient;
 use maplibre::platform::schedule_method::TokioScheduleMethod;
 use maplibre::window::FromWindow;
 use maplibre::MapBuilder;
+use maplibre_winit::winit::{WinitEventLoop, WinitMapWindow, WinitWindow};
 
 #[cfg(not(any(target_os = "macos", target_os = "ios")))]
 compile_error!("apple works only on macOS and iOS.");
@@ -10,7 +11,8 @@ compile_error!("apple works only on macOS and iOS.");
 pub fn maplibre_apple_main() {
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
-    MapBuilder::from_window("A fantastic window!")
+    let builder: MapBuilder<WinitMapWindow, _, _, _> = MapBuilder::new();
+    builder
         .with_http_client(ReqwestHttpClient::new(None))
         .with_schedule_method(TokioScheduleMethod::new())
         .build()
