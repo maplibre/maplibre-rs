e76b2d000a345dc441f0e4250a030924b935a693
Maximilian Ammann <max@maxammann.org>
2022-06-03 17:30:11 +0200

    Fix benchmarks

--- maplibre/build.rs
+++ maplibre/build.rs
@@ -3,27 +3,11 @@
 //! This script is built and executed just before building the package.
 //! It will validate the WGSL (WebGPU Shading Language) shaders and embed static files.
 
-#[cfg(feature = "embed-static-tiles")]
-use maplibre_build_tools::mbtiles::extract;
 use maplibre_build_tools::wgsl::validate_project_wgsl;
 
 const MUNICH_X: u32 = 17425;
 const MUNICH_Y: u32 = 11365;
 const MUNICH_Z: u8 = 15;
-
-/// Tiles which can be used by StaticTileFetcher.
-#[cfg(feature = "embed-static-tiles")]
-fn clean_static_tiles() -> std::path::PathBuf {
-    let out_dir = std::env::var("OUT_DIR").unwrap();
-
-    let out = std::path::Path::new(&out_dir).join("extracted-tiles");
-
-    if out.exists() && out.is_dir() {
-        std::fs::remove_dir_all(&out).unwrap()
-    }
-
-    out
-}
 
 /*use std::fs::File;
 use std::io::BufReader;
@@ -47,18 +31,32 @@ fn generate_type_def() -> Option<u32> {
     Some(5)
 }
 
-#[cfg(feature = "embed-static-tiles")]
 fn embed_tiles_statically() {
+    use maplibre_build_tools::mbtiles::extract;
     use std::env;
     use std::path::Path;
+
+    /// Tiles which can be used by StaticTileFetcher.
+    fn clean_static_tiles() -> std::path::PathBuf {
+        let out_dir = std::env::var("OUT_DIR").unwrap();
+
+        let out = std::path::Path::new(&out_dir).join("extracted-tiles");
+
+        if out.exists() && out.is_dir() {
+            std::fs::remove_dir_all(&out).unwrap()
+        }
+
+        out
+    }
+
     let out = clean_static_tiles();
 
     let root_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    let source = Path::new(&root_dir).join(format!("test-data/munich-{}.mbtiles", MUNICH_Z));
+    let source = Path::new(&root_dir).join(format!("../test-data/munich-{}.mbtiles", MUNICH_Z));
 
     if source.exists() {
-        println!("cargo:rustc-cfg=static_tiles");
+        println!("cargo:rustc-cfg=static_tiles_found");
         // Pack tiles around Munich HBF (100 tiles in each direction)
         extract(
             source,
@@ -76,6 +74,5 @@ fn embed_tiles_statically() {
 fn main() {
     validate_project_wgsl();
 
-    #[cfg(feature = "embed-static-tiles")]
     embed_tiles_statically();
 }
