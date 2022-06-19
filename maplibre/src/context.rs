1c32043766f8eea8834ef85dddb3ff2f4204f6d2
Drabble <antoine.drabble@gmail.com>
2022-06-19 12:57:31 +0200

    Use tracing instead of log for zoom inputs

--- maplibre/src/context.rs
+++ maplibre/src/context.rs
@@ -58,7 +58,7 @@ impl ViewState {
 
     pub fn update_zoom(&mut self, new_zoom: Zoom) {
         *self.zoom = new_zoom;
-        log::info!("zoom: {}", new_zoom);
+        tracing::info!("zoom: {}", new_zoom);
     }
 }
 
