27580f67332428bb644788087f9b8090e040ed58
Maximilian Ammann <max@maxammann.org>
2022-10-29 16:51:40 +0200

    Generage SUMMARY.md file

--- docs/generate-summary.sh
+++ docs/generate-summary.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-printf -- '- [RFCs](rfc/0001-rfc-process.md)\n\n' >> src/SUMMARY.md
+printf -- '- [RFCs](rfc/0001-rfc-process.md)\n\n' > src/SUMMARY-rfc.md
 
 find ./src/rfc ! -type d -name '*.md' -print0 \
   | sort -z \
   | while read -r -d '' file;
 do
     printf -- '  - [%s](rfc/%s)\n' "$(basename "$file" ".md")" "$(basename "$file")"
-done >> src/SUMMARY.md
+done >> src/SUMMARY-rfc.md
 
+cat src/SUMMARY-book.md src/SUMMARY-rfc.md > src/SUMMARY.md
