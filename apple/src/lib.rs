b850c8854b1bac14ce666abb259733dc130083a4
Maximilian Ammann <max@maxammann.org>
2022-11-03 08:31:22 +0100

    Clippy fixes

--- apple/src/lib.rs
+++ apple/src/lib.rs
@@ -1,12 +1,6 @@
 #![deny(unused_imports)]
 
-use maplibre::{
-    io::apc::SchedulerAsyncProcedureCall,
-    platform::{http_client::ReqwestHttpClient, run_multithreaded, scheduler::TokioScheduler},
-};
-use maplibre_winit::{
-    run_headed_map, WinitEnvironment, WinitEventLoop, WinitMapWindow, WinitMapWindowConfig,
-};
+use maplibre_winit::run_headed_map;
 
 #[cfg(not(any(target_os = "macos", target_os = "ios")))]
 compile_error!("apple works only on macOS and iOS.");
