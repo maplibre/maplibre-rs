c7289c65ab32e1dd4a9668d284933babb331d629
Maximilian Ammann <max@maxammann.org>
2022-12-15 11:30:56 +0100

    Add thread names

--- maplibre/src/platform/noweb/mod.rs
+++ maplibre/src/platform/noweb/mod.rs
@@ -1,6 +1,9 @@
 //! Module which is used target platform is not web related.
 
-use std::future::Future;
+use std::{
+    future::Future,
+    sync::atomic::{AtomicUsize, Ordering},
+};
 
 pub mod http_client;
 pub mod scheduler;
@@ -8,12 +11,18 @@ pub mod trace;
 
 pub fn run_multithreaded<F: Future>(future: F) -> F::Output {
     tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(4)
         .enable_io()
         .enable_time()
+        .thread_name_fn(|| {
+            static ATOMIC_ID: AtomicUsize = AtomicUsize::new(0);
+            let id = ATOMIC_ID.fetch_add(1, Ordering::SeqCst);
+            format!("maplibre-rs-pool-{}", id)
+        })
         .on_thread_start(|| {
             #[cfg(feature = "trace")]
             tracing_tracy::client::set_thread_name!("tokio-runtime-worker");
+
+            log::info!("Worker thread started")
         })
         .build()
         .unwrap()
