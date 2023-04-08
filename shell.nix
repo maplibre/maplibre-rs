427a92458101296fdf3db5dbe7c28fd9905bcbb1
Max Ammann <max@maxammann.org>
2023-04-08 17:11:45 -0400

    Cleanup shell

--- shell.nix
+++ shell.nix
@@ -1,12 +1,31 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = [
-      pkgs.rustup
-      pkgs.just
-      pkgs.flatbuffers pkgs.protobuf
-      pkgs.libiconv
-      pkgs.darwin.apple_sdk.frameworks.ApplicationServices
-      pkgs.darwin.apple_sdk.frameworks.CoreVideo
-      pkgs.darwin.apple_sdk.frameworks.AppKit
-    ];
+# This nix-shell only supports macOS right now. Soon I will also add support for Linux
+# The repository supports direnv (https://direnv.net/). If your IDE supports direnv,
+# then you do not need to care about dependencies.
+
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+let
+  unstable = import
+    (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/075dce259f6ced5cee1226dd76474d0674b54e64.tar.gz";
+    })
+    { };
+in
+pkgs.mkShell {
+  nativeBuildInputs = [
+    # Tools
+    unstable.rustup
+    unstable.just
+    unstable.tracy
+    unstable.nixpkgs-fmt # To format this file: nixpkgs-fmt *.nix
+    # System dependencies
+    unstable.flatbuffers
+    unstable.protobuf
+  ]
+  ++ lib.optionals stdenv.isDarwin [
+    unstable.libiconv
+    pkgs.darwin.apple_sdk.frameworks.ApplicationServices
+    pkgs.darwin.apple_sdk.frameworks.CoreVideo
+    pkgs.darwin.apple_sdk.frameworks.AppKit
+  ];
 }
