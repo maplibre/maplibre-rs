aec167d211549c1ae08d5d8c107e5ea00d49f4ac
Max Ammann <max@maxammann.org>
2023-04-15 17:46:58 -0400

    Fix comments

--- maplibre/src/coords.rs
+++ maplibre/src/coords.rs
@@ -147,20 +147,16 @@ impl LatLon {
         }
     }
 
-    /*
-     * Approximate radius of the earth in meters.
-     * Uses the WGS-84 approximation. The radius at the equator is ~6378137 and at the poles is ~6356752. https://en.wikipedia.org/wiki/World_Geodetic_System#WGS84
-     * 6371008.8 is one published "average radius" see https://en.wikipedia.org/wiki/Earth_radius#Mean_radius, or ftp://athena.fsv.cvut.cz/ZFG/grs80-Moritz.pdf p.4
-     */
+    /// Approximate radius of the earth in meters.
+    /// Uses the WGS-84 approximation. The radius at the equator is ~6378137 and at the poles is ~6356752. https://en.wikipedia.org/wiki/World_Geodetic_System#WGS84
+    /// 6371008.8 is one published "average radius" see https://en.wikipedia.org/wiki/Earth_radius#Mean_radius, or ftp://athena.fsv.cvut.cz/ZFG/grs80-Moritz.pdf p.4
     const EARTH_RADIUS: f64 = 6371008.8;
-    /*
-     * The average circumference of the world in meters.
-     */
+
+    /// The average circumference of the world in meters.
+
     const EARTH_CIRCUMFRENCE: f64 = 2.0 * PI * Self::EARTH_RADIUS; // meters
 
-    /*
-     * The circumference at a line of latitude in meters.
-     */
+    /// The circumference at a line of latitude in meters.
     fn circumference_at_latitude(&self) -> f64 {
         Self::EARTH_CIRCUMFRENCE * (self.latitude * PI / 180.0).cos()
     }
