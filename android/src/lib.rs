0b332bfdec040deb5a83c535a23cff16993cba3e
Max Ammann <maximilian.ammann@trailofbits.com>
2023-10-02 18:25:53 +0100

    Upgrade winit to window resizes on macOS

--- android/src/lib.rs
+++ android/src/lib.rs
@@ -1,7 +1,5 @@
 #![deny(unused_imports)]
 
-use std::ffi::CString;
-
 use jni::{objects::JClass, JNIEnv};
 use log::Level;
 use maplibre_winit::run_headed_map;
@@ -9,8 +7,9 @@ use maplibre_winit::run_headed_map;
 #[cfg(not(any(no_pendantic_os_check, target_os = "android")))]
 compile_error!("android works only on android.");
 
-#[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
+#[no_mangle]
 pub fn android_main() {
+    android_logger::init_once(android_logger::Config::default().with_min_level(Level::Info));
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
     // TODO: Maybe requires: Some(Backends::VULKAN)
@@ -22,7 +21,5 @@ pub extern "system" fn Java_org_maplibre_1rs_MapLibreRs_android_1main(
     _env: JNIEnv,
     _class: JClass,
 ) {
-    let tag = CString::new("maplibre").unwrap();
-    let message = CString::new("maplibre WOORKING").unwrap();
-    ndk_glue::android_log(Level::Warn, &tag, &message);
+    log::log!(Level::Warn, "maplibre WOORKING");
 }
