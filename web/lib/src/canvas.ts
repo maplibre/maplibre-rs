88836621870b87ec25080f5ba4e6f2a9dde5d704
Max Ammann <max@maxammann.org>
2023-10-20 21:51:28 +0200

    Disable right clicks on canvas

--- web/lib/src/canvas.ts
+++ web/lib/src/canvas.ts
@@ -2,5 +2,6 @@ export const preventDefaultTouchActions = () => {
     document.body.querySelectorAll("canvas").forEach(canvas => {
         canvas.addEventListener("touchstart", e => e.preventDefault())
         canvas.addEventListener("touchmove", e => e.preventDefault())
+        canvas.addEventListener("contextmenu", e => e.preventDefault())
     })
 }
