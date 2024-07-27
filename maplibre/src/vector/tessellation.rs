621371ac5b2d4aee601327ac9737770532f80578
Max Ammann <max@maxammann.org>
2024-07-27 23:19:14 +0200

    Move tesselation code to vector module

--- maplibre/src/vector/tessellation.rs
+++ maplibre/src/vector/tessellation.rs
@@ -1,11 +1,10 @@
-//! Tessellator implementation.
+//! Tessellation for lines and polygons is implemented here.
 
 use std::cell::RefCell;
 
 use geozero::{FeatureProcessor, GeomProcessor, PropertyProcessor};
 use lyon::{
     geom,
-    lyon_tessellation::VertexBuffers,
     path::{path::Builder, Path},
     tessellation::{
         geometry_builder::MaxIndex, BuffersBuilder, FillOptions, FillRule, FillTessellator,
@@ -15,8 +14,115 @@ use lyon::{
 
 use crate::{
     render::ShaderVertex,
-    tessellation::{VertexConstructor, DEFAULT_TOLERANCE},
 };
+
+use bytemuck::Pod;
+use lyon::tessellation::{
+    FillVertex, FillVertexConstructor, StrokeVertex, StrokeVertexConstructor, VertexBuffers,
+};
+
+const DEFAULT_TOLERANCE: f32 = 0.02;
+
+/// Vertex buffers index data type.
+pub type IndexDataType = u32; // Must match INDEX_FORMAT
+
+/// Constructor for Fill and Stroke vertices.
+pub struct VertexConstructor {}
+
+impl FillVertexConstructor<ShaderVertex> for VertexConstructor {
+    fn new_vertex(&mut self, vertex: FillVertex) -> ShaderVertex {
+        ShaderVertex::new(vertex.position().to_array(), [0.0, 0.0])
+    }
+}
+
+impl StrokeVertexConstructor<ShaderVertex> for VertexConstructor {
+    fn new_vertex(&mut self, vertex: StrokeVertex) -> ShaderVertex {
+        ShaderVertex::new(
+            vertex.position_on_path().to_array(),
+            vertex.normal().to_array(),
+        )
+    }
+}
+
+/// Vertex buffer which includes additional padding to fulfill the `wgpu::COPY_BUFFER_ALIGNMENT`.
+#[derive(Clone)]
+pub struct OverAlignedVertexBuffer<V, I> {
+    pub buffer: VertexBuffers<V, I>,
+    pub usable_indices: u32,
+}
+
+impl<V, I> OverAlignedVertexBuffer<V, I> {
+    pub fn empty() -> Self {
+        Self {
+            buffer: VertexBuffers::with_capacity(0, 0),
+            usable_indices: 0,
+        }
+    }
+
+    pub fn from_iters<IV, II>(vertices: IV, indices: II, usable_indices: u32) -> Self
+    where
+        IV: IntoIterator<Item=V>,
+        II: IntoIterator<Item=I>,
+        IV::IntoIter: ExactSizeIterator,
+        II::IntoIter: ExactSizeIterator,
+    {
+        let vertices = vertices.into_iter();
+        let indices = indices.into_iter();
+        let mut buffers = VertexBuffers::with_capacity(vertices.len(), indices.len());
+        buffers.vertices.extend(vertices);
+        buffers.indices.extend(indices);
+        Self {
+            buffer: buffers,
+            usable_indices,
+        }
+    }
+}
+
+impl<V: Pod, I: Pod> From<VertexBuffers<V, I>> for OverAlignedVertexBuffer<V, I> {
+    fn from(mut buffer: VertexBuffers<V, I>) -> Self {
+        let usable_indices = buffer.indices.len() as u32;
+        buffer.align_vertices();
+        buffer.align_indices();
+        Self {
+            buffer,
+            usable_indices,
+        }
+    }
+}
+
+trait Align<V: Pod, I: Pod> {
+    fn align_vertices(&mut self);
+    fn align_indices(&mut self);
+}
+
+impl<V: Pod, I: Pod> Align<V, I> for VertexBuffers<V, I> {
+    fn align_vertices(&mut self) {
+        let align = wgpu::COPY_BUFFER_ALIGNMENT;
+        let stride = std::mem::size_of::<ShaderVertex>() as wgpu::BufferAddress;
+        let unpadded_bytes = self.vertices.len() as wgpu::BufferAddress * stride;
+        let padding_bytes = (align - unpadded_bytes % align) % align;
+
+        if padding_bytes != 0 {
+            panic!(
+                "vertices are always aligned to wgpu::COPY_BUFFER_ALIGNMENT \
+                    because GpuVertexUniform is aligned"
+            )
+        }
+    }
+
+    fn align_indices(&mut self) {
+        let align = wgpu::COPY_BUFFER_ALIGNMENT;
+        let stride = std::mem::size_of::<I>() as wgpu::BufferAddress;
+        let unpadded_bytes = self.indices.len() as wgpu::BufferAddress * stride;
+        let padding_bytes = (align - unpadded_bytes % align) % align;
+        let overpad = (padding_bytes + stride - 1) / stride; // Divide by stride but round up
+
+        for _ in 0..overpad {
+            self.indices.push(I::zeroed());
+        }
+    }
+}
+
 
 type GeoResult<T> = geozero::error::Result<T>;
 
@@ -33,7 +139,7 @@ pub struct ZeroTessellator<I: std::ops::Add + From<lyon::tessellation::VertexId>
 }
 
 impl<I: std::ops::Add + From<lyon::tessellation::VertexId> + MaxIndex> Default
-    for ZeroTessellator<I>
+for ZeroTessellator<I>
 {
     fn default() -> Self {
         Self {
@@ -88,7 +194,7 @@ impl<I: std::ops::Add + From<lyon::tessellation::VertexId> + MaxIndex> ZeroTesse
 }
 
 impl<I: std::ops::Add + From<lyon::tessellation::VertexId> + MaxIndex> GeomProcessor
-    for ZeroTessellator<I>
+for ZeroTessellator<I>
 {
     fn xy(&mut self, x: f64, y: f64, _idx: usize) -> GeoResult<()> {
         // log::info!("xy");
@@ -186,12 +292,11 @@ impl<I: std::ops::Add + From<lyon::tessellation::VertexId> + MaxIndex> GeomProce
 }
 
 impl<I: std::ops::Add + From<lyon::tessellation::VertexId> + MaxIndex> PropertyProcessor
-    for ZeroTessellator<I>
-{
-}
+for ZeroTessellator<I>
+{}
 
 impl<I: std::ops::Add + From<lyon::tessellation::VertexId> + MaxIndex> FeatureProcessor
-    for ZeroTessellator<I>
+for ZeroTessellator<I>
 {
     fn feature_end(&mut self, _idx: u64) -> geozero::error::Result<()> {
         self.update_feature_indices();
